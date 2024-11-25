37a9c64cc36e698c238039fd34e84410b8caa5f6
Youngjae Moon <pingumoon18@gmail.com>
2024-11-25 11:22:52 -0600

    dockerfile update

---
+++ vm1/app/run_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python3 test.py
+python3 database_setup.py
+python3 test_unit.py
+python3 plot_metrics_unit.py
+python3 test_aflplusplus.py
+python3 plot_metrics_aflplusplus.py
+python3 test_atheris.py
+python3 plot_metrics_atheris.py
